29e75a1af3fe086621d2af8f2e12392a81da2f1e
Eugene Martinson <evgeny.martinson@gmail.com>
2023-10-13 17:40:38 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhenixSdk",
-            url: "https://dl.phenixrts.com/sdk/ios/2023.0.0-7dad768-20230925T102932677/PhenixSdk.xcframework.zip",
-            checksum: "a216d2be5f636baf0b33f2668006c4a030055c2f1a357e785b0357a9bc41234a"
+            url: "https://firebasestorage.googleapis.com/v0/b/phillips-dev.appspot.com/o/PhenixSdk.xcframework.zip?alt=media&token=02d5e883-63b5-4ce5-bcc5-b9a07e687612&_gl=1*e7z3cj*_ga*ODE5OTAwMTEzLjE2OTM3MTc1NjM.*_ga_CW55HF8NVT*MTY5NzE5NDE2MC4zOS4xLjE2OTcxOTQxNjcuNTMuMC4w",
+            checksum: "2ecc8dbfb76b6ce27cf62b8de879e12f33084f55102b9f6a46edba16840d9246"
         ),
     ]
 )
